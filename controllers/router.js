d12adaca3f3a6d9258e2681d6fc8f067c2c7e232
Brandon Kelley <14bmkelley@gmail.com>
2015-09-27 12:13:15 -0700

    contact form

--- controllers/router.js
+++ controllers/router.js
@@ -19,11 +19,44 @@ module.exports = function(router) {
   });
   
   router.get("/contact_me", function(request, response) {
-    response.render("template", {
-      "title": "Contact Me"
+    parseQuery(request.url, function(query) {
+      var submitted = (query["success"]) ? true : false;
+      response.render("template", {
+        "title": "Contact Me",
+        "success": submitted
+      });
     });
   });
   
   return router;
   
 };
+
+var parseQuery = function(url, callback) {
+  var query = {};
+  var keyStart = false;
+  var key = "";
+  var valStart = false;
+  var val = "";
+  for (var i = 0; i < url.length; i++) {
+    if (url[i] === "?" || url[i] === "&") {
+      keyStart = true;
+      valStart = false;
+      if (key !== "" && val !== "") {
+        query[key] = val;
+        key = "";
+        val = "";
+      }
+    } else if (url[i] === "=") {
+      keyStart = false;
+      valStart = true;
+    } else if (i === url.length - 1 && url[i] !== "/") {
+      val += url[i];
+      query[key] = val;
+    } else {
+      if (keyStart) key += url[i];
+      if (valStart) val += url[i];
+    }
+  }
+  callback(query);
+}
--- public/res/js/main.js
+++ public/res/js/main.js
@@ -1,5 +1,26 @@
 $(document).ready(function() {
-    
-   // nothing yet 
-   
+  
+  $("#focus").focus();
+  
+  $("#submit").keypress(function(event) {
+    if (event.keyCode === 13) {
+      event.preventDefault();
+      submitForm();
+    }
+  });
+  
+  $("button").click(function() {
+    submitForm();
+  });
+  
+  var submitForm = function() {
+    var name = $("input[type='name']").val();
+    var email = $("input[type='email']").val();
+    var message = $("textarea").val();
+    console.log("name: " + name);
+    console.log("email: " + email);
+    console.log("message: " + message);
+    window.location = "/contact_me?success=1";
+  }
+  
 });
